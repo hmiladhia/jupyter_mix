baafa08774f4573dc6887fc8db2a296a53e1e0ab
Dhia Hmila <dhiahmila@gmail.com>
2021-03-27 00:12:03 +0100

    added comments & fixes

--- script1.py
+++ script1.py
@@ -10,11 +10,14 @@ def write_ipynb(notebook, notebook_path):
         json.dump(notebook, f)
 
 # Reading the notebooks
-first_notebook = read_ipynb('Untitled1.ipynb')
-second_notebook = read_ipynb('Untitled2.ipynb')
+first_notebook = read_ipynb('first_notebook.ipynb')
+second_notebook = read_ipynb('second_notebook.ipynb')
 
 # Copying the notebook
 final_notebook = copy.deepcopy(first_notebook)
+
+# Concatenating the notebooks
+final_notebook['cells'] = first_notebook['cells'] + second_notebook['cells']
 
 # Saving the new notebook 
 write_ipynb(final_notebook, 'final_notebook.ipynb')
--- script2.py
+++ script2.py
@@ -1,8 +1,14 @@
 import nbformat
 
-first_notebok = nbformat.read('first_notebok.ipynb', 4)
-second_notebok = nbformat.read('second_notebok.ipynb', 4)
+# Reading the notebooks
+first_notebook = nbformat.read('first_notebook.ipynb', 4)
+second_notebook = nbformat.read('second_notebook.ipynb', 4)
 
-nb.cells = first_notebok.cells + second_notebok.cells
+# Creating a new notebook
+final_notebook = nbformat.v4.new_notebook(metadata=first_notebook.metadata)
 
-nbformat.write(nb, 'final_notebook.ipynb')
+# Concatenating the notebooks
+final_notebook.cells = first_notebook.cells + second_notebook.cells
+
+# Saving the new notebook 
+nbformat.write(final_notebook, 'final_notebook.ipynb')
